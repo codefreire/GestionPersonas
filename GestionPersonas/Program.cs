1640c244979548495fb8e2705d105a2f7782fb9e
Carlos Freire <cestebanfreire@hotmail.com>
2024-10-17 10:07:27 -0500

    Quitar Home controller y vistas

--- GestionPersonas/Program.cs
+++ GestionPersonas/Program.cs
@@ -12,14 +12,6 @@ builder.Services.AddDbContext<PersonasDBContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -29,6 +21,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Personas}/{action=Index}/{id?}");
 
 app.Run();
